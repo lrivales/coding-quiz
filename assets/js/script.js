5dc429521fec022d9189223f8f39bb57be4e3ed3
Lawrence Rivales <lrivales@gmail.com>
2021-09-09 16:59:04 -0400

    pseudocode added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,22 @@
-var quizContainer = document.querySelector(".main-form");
-var resultsContainer = document.querySelector(".results");
 var btnEl = document.querySelector(".btn");
-var data = [
+var answerBtnEl = document.querySelector(".btn[id='answerBtn'");
+
+var myQuestions = [
     {
-        question: "1st question",
-        answers: {
-            a: "first answer",
-            b: "second answer",
-            c: "third answer"
-        },
-        correctAnswer: "a"
+        question: "1+1",
+        a: "2",
+        b: "1",
+        c: "3",
+        correctAnswer: "2"
     },
     {
-        question: "2nd question",
-        answers: {
-            a: "first answer",
-            b: "second answer",
-            c: "third answer"
-        },
-        correctAnswer: "b"
+        question: "2+2",
+        a: "3",
+        b: "4",
+        c: "6",
+        correctAnswer: "4"
     }
 ];
-
-var buildQuiz = function() {
-    // remove intro h1 and start quiz button
-    document.querySelector("#intro").remove();
-    document.querySelector("#start-quiz").remove();
-
-    var output=[];
-
-    for (var i = 0; i < data.length; i++) {
-        var userSelection = "";
-        
-         // write question on page
-         createQuestionEl(data[i].question);
-
-        for (letter in data[i].answers) {
-            createBtnEl(data[i].answers);
-        }
-    }
-};
-
-var showResults = function() {
-
-};
 
 var createQuestionEl = function(questionTxtContent) {
     var question = document.createElement("h1");
@@ -57,8 +30,39 @@ var createBtnEl = function(btnTxtContent) {
     var btn = document.createElement("button");
     btn.className = "btn";
     btn.textContent = btnTxtContent;
+    btn.setAttribute("id","answerBtn");
     console.log(btn);
     document.querySelector(".main-form").appendChild(btn);
 };
 
-btnEl.addEventListener("click", buildQuiz);
+var startQuiz = function() {
+    var x = 0;
+
+    //remove old elements
+    document.querySelector(".page-title").remove();
+    document.querySelector(".btn").remove();
+
+    // write question
+    createQuestionEl(myQuestions[x].question);
+    
+    // add buttons for answers
+    createBtnEl(myQuestions[x].a);
+    createBtnEl(myQuestions[x].b);
+    createBtnEl(myQuestions[x].c);
+
+    // get selected answer
+    // answerBtnEl.addEventListener("click", function() {
+    //     var selectedAnswer = answerBtnEl.textContent;
+    //     console.log(selectedAnswer);
+    // });
+
+    // compare selected answer with correct answer
+
+    // go to next question
+ 
+    // if there are no more questions, end the quiz
+
+    // display the score and the highest score
+};
+
+btnEl.addEventListener("click", startQuiz);
